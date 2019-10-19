52654c1db5391bd8a72b65652922a744a040dab4
Dirk Lemstra <dirk@lemstra.org>
2019-10-19 21:20:33 +0200

    Write certificate to a file.

--- dist/index.js
+++ dist/index.js
@@ -339,11 +339,13 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core = __importStar(__webpack_require__(470));
+const fs_1 = __webpack_require__(747);
 async function run() {
     try {
         const base64Certificate = core.getInput('certificate');
         const certificate = Buffer.from(base64Certificate, 'base64');
-        console.log(certificate.length);
+        console.log(`Writing ${certificate.length} bytes to certificate.pfx.`);
+        await fs_1.promises.writeFile('./certificate.pfx', certificate);
     }
     catch (err) {
         core.setFailed(`Action failed with error ${err}`);
@@ -358,6 +360,13 @@ run();
 /***/ (function(module) {
 
 module.exports = require("path");
+
+/***/ }),
+
+/***/ 747:
+/***/ (function(module) {
+
+module.exports = require("fs");
 
 /***/ })
 
--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import * as core from '@actions/core';
+import { promises as fs } from 'fs';
 
 async function run() {
     try {
         const base64Certificate = core.getInput('certificate');
         const certificate = Buffer.from(base64Certificate, 'base64');
-        console.log(certificate.length);
+        console.log(`Writing ${certificate.length} bytes to certificate.pfx.`);
+        await fs.writeFile('./certificate.pfx', certificate);
     }
     catch (err) {
         core.setFailed(`Action failed with error ${err}`);

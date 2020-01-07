cbb8a2a6c8e17dd5d438abe5ce3ba52b8882233f
Dirk Lemstra <dirk@lemstra.org>
2020-01-07 22:35:47 +0100

    Added support for signing NuGet packages.

--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
 import * as core from '@actions/core';
 import { promises as fs } from 'fs';
+import { existsSync, createWriteStream } from 'fs';
+import https from 'https';
 import util from 'util';
 import { exec } from 'child_process';
 import { env } from 'process';
 
 const asyncExec = util.promisify(exec);
 const certificateFileName = env['TEMP'] + '\\certificate.pfx';
+const nugetFileName = env['TEMP'] + '\\nuget.exe';
+
+const timestampUrl = 'http://timestamp.digicert.com';
+const signtool = 'C:/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x86/signtool.exe';
 
 function sleep(seconds: number) {
     if (seconds > 0)
@@ -22,15 +28,47 @@ async function createCertificatePfx() {
     await fs.writeFile(certificateFileName, certificate);
 }
 
-async function signFile(fileName: string) {
-    const signtool = 'C:/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x86/signtool.exe';
-    const timestampUrl = 'http://timestamp.digicert.com';
+async function downloadNuGet() {
+    return new Promise(resolve => {
+        if (existsSync(nugetFileName)) {
+            resolve();
+            return;
+        }
 
+        console.log(`Downloading nuget.exe.`);
+
+        const file = createWriteStream(nugetFileName);
+        https.get('https://dist.nuget.org/win-x86-commandline/latest/nuget.exe', (response) => {
+            response.pipe(file);
+            file.on('finish', function() {
+                file.close();
+                resolve();
+            });
+        });
+    });
+}
+
+async function signDll(fileName: string) {
     try {
         const { stdout } = await asyncExec(`"${signtool}" sign /f ${certificateFileName} /tr ${timestampUrl} /td sha256 /fd sha256 ${fileName}`);
         console.log(stdout);
         return true;
     } catch(err) {
+        console.log(err.stdout);
+        console.log(err.stderr);
+        return false;
+    }
+}
+
+async function signNupkg(fileName: string) {
+    await downloadNuGet();
+
+    try {
+        const { stdout } = await asyncExec(`"${nugetFileName}" sign ${fileName} -CertificatePath ${certificateFileName} -Timestamper ${timestampUrl}`);
+        console.log(stdout);
+        return true;
+    } catch(err) {
+        console.log(err.stdout);
         console.log(err.stderr);
         return false;
     }
@@ -40,8 +78,13 @@ async function trySignFile(fileName: string) {
     console.log(`Signing ${fileName}.`);
     for (let i=0; i< 10; i++) {
         await sleep(i);
-        if (await signFile(fileName))
-            return;
+        if (fileName.endsWith('.dll')) {
+            if (await signDll(fileName))
+                return;
+        } else if (fileName.endsWith('.nupkg')) {
+            if (await signNupkg(fileName))
+                return;
+        }
     }
     throw `failed to sign '${fileName}'`;
 }
@@ -51,10 +94,11 @@ async function* getFiles(folder: string, recursive: boolean): any {
     for (const file of files) {
         const fullPath = `${folder}/${file}`;
         const stat = await fs.stat(fullPath);
-        if (stat.isFile() && file.endsWith('.dll')) {
-            yield fullPath;
+        if (stat.isFile()) {
+            if (file.endsWith('.dll') || file.endsWith('.nupkg'))
+                yield fullPath;
         }
-        if (stat.isDirectory() && recursive) {
+        else if (stat.isDirectory() && recursive) {
             yield* getFiles(fullPath, recursive);
         }
     }

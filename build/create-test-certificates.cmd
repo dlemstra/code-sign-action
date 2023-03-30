9cf1606539bc4a976376ff27e037d4b165b72b89
Dirk Lemstra <dirk@lemstra.org>
2023-03-30 22:44:17 +0200

    Use test certificate instead of real certificate.

---
+++ build/create-test-certificates.cmd
@@ -0,0 +1,21 @@
+@echo off
+
+openssl genrsa -des3 -out ca.key -passout pass:casecret 2048
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=Root Certificate" -passin pass:casecret -sha256 -days 365 -out ca.pem
+openssl x509 -outform der -in ca.pem -out ca.crt
+certutil -addstore Root ca.crt
+
+openssl req -nodes -newkey rsa:2048 -days 365 -subj "/CN=Code Signing Certificate" -keyout sign.key -out sign.csr
+openssl x509 -req -CA ca.pem -CAkey ca.key -passin pass:casecret -in sign.csr -days 365 -CAcreateserial -extfile v3.ext -out sign.crt
+
+openssl pkcs12 -export -passout pass: -inkey sign.key -in sign.crt -out certificate_without_password.pfx
+certutil -encode certificate_without_password.pfx certificate_without_password.txt
+echo CERTIFICATE_WITHOUT_PASSWORD^<^<EOF >> %GITHUB_ENV%
+type certificate_without_password.txt >> %GITHUB_ENV%
+echo EOF >> %GITHUB_ENV%
+
+openssl pkcs12 -export -passout pass:secret -inkey sign.key -in sign.crt -out certificate_with_password.pfx
+certutil -encode certificate_with_password.pfx certificate_with_password.txt
+echo CERTIFICATE_WITH_PASSWORD^<^<EOF >> %GITHUB_ENV%
+type certificate_with_password.txt >> %GITHUB_ENV%
+echo EOF >> %GITHUB_ENV%
